6488e9e7934db22cee72bceb1bb449b9f701274d
maim21 <sh2107368@qu.edu.qa>
2025-03-09 14:33:30 +0300

    Nutrition calculation through firebase

--- lib/models/users.dart
+++ lib/models/users.dart
@@ -1,28 +1,33 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Users {
+  final String uid;
   final String email;
   final String gender;
+  final String name;
   final int age;
   final double weight; // in kg
   final double height; // in cm
   final int workoutDays; // 1-6
+  final String goal;
   final DateTime createdAt;
   final DateTime updatedAt;
 
   Users({
+    required this.uid,
     required this.email,
     required this.gender,
+    required this.name,
     required this.age,
     required this.weight,
     required this.height,
     required this.workoutDays,
+    required this.goal,
     required this.createdAt,
     required this.updatedAt,
   });
 
-  // BMR calculation
-  double calculateBMR() {
+  double calculateBMR(String gender) {
     if (gender.toLowerCase() == 'male') {
       return (10 * weight) + (6.25 * height) - (5 * age) + 5;
     } else {
@@ -31,35 +36,36 @@ class Users {
   }
 
   // TDEE calculation
-  double calculateTDEE(double bmr, int workoutDays) {
+  double calculateTDEE(double bmr, int workoutDays, String goal) {
     double multiplier;
-
+    double cal = 0;
     if (workoutDays == 1) {
       multiplier = 1.2; // 1 day a week
     } else if (workoutDays >= 2 || workoutDays <= 3) {
       multiplier = 1.3; // 2-3 days a week
     } else if (workoutDays >= 4 || workoutDays <= 5) {
       multiplier = 1.5; // 4-5 days a week
-    } else { // 6 days a week
+    } else {
+      // 6 days a week
       multiplier = 1.9;
     }
-    return bmr * multiplier; // Calories
-  }
-
-
+    if (goal == 'Weight Loss') {
+      cal = (bmr * multiplier) - 300;
+    } else {
+      cal = bmr * multiplier;
+    }
+    return cal;
+  } // Calories
 
   // Macronutrient calculation based on TDEE and goals
   Map<String, double> calculateMacronutrients(String goal, double bmr, int workoutDays) {
-    double tdee = calculateTDEE(bmr, workoutDays);
+    double tdee = calculateTDEE(bmr, workoutDays, goal);
     Map<String, double> macros = {};
-
-    if (goal == 'Weight Loss') {
-      tdee -= 300; // Deficit for weight loss
-    }
 
     switch (goal) {
       case 'Weight Loss':
         macros = {
+          'calories':(tdee),
           'carbs': (tdee * 0.45) / 4, // carbs per gram = 4 cal
           'protein': (tdee * 0.30) / 4, // protein per gram = 4 cal
           'fat': (tdee * 0.25) / 9, // fat per gram = 9 cal
@@ -68,6 +74,7 @@ class Users {
 
       case 'Gain Muscle':
         macros = {
+          'calories':(tdee),
           'carbs': (tdee * 0.45) / 4,
           'protein': (tdee * 0.35) / 4,
           'fat': (tdee * 0.20) / 9,
@@ -76,6 +83,7 @@ class Users {
 
       case 'Improve Fitness':
         macros = {
+          'calories':(tdee),
           'carbs': (tdee * 0.60) / 4,
           'protein': (tdee * 0.15) / 4,
           'fat': (tdee * 0.25) / 9,
@@ -89,12 +97,15 @@ class Users {
   // Firestore serialization
   Map<String, dynamic> toMap() {
     return {
+      'uid': uid,  // Include uid in the map for Firestore storage
       'email': email,
       'gender': gender,
+      'name': name,
       'age': age,
       'weight': weight,
       'height': height,
       'workoutDays': workoutDays,
+      'goal': goal,
       'createdAt': createdAt,
       'updatedAt': updatedAt,
     };
@@ -102,14 +113,17 @@ class Users {
 
   factory Users.fromMap(Map<String, dynamic> map) {
     return Users(
-      email: map['email'],
-      gender: map['gender'],
-      age: map['age'],
-      weight: map['weight'],
-      height: map['height'],
-      workoutDays: map['workoutDays'],
-      createdAt: (map['createdAt'] as Timestamp).toDate(),
-      updatedAt: (map['updatedAt'] as Timestamp).toDate(),
+      uid: map['uid'] ?? '',  // Ensure the uid is handled when retrieving from Firestore
+      email: map['email'] ?? '',
+      gender: map['gender'] ?? '',
+      name: map['name'] ?? '',
+      age: map['age'] ?? 16,
+      weight: map['weight']?.toDouble() ?? 0.0,
+      height: map['height']?.toDouble() ?? 0.0,
+      workoutDays: map['workoutDays'] ?? 1,
+      goal: map['goal'] ?? '',
+      createdAt: (map['createdAt'] as Timestamp?)?.toDate() ?? DateTime.now(), // Handle null or missing createdAt
+      updatedAt: (map['updatedAt'] as Timestamp?)?.toDate() ?? DateTime.now(), // Handle null or missing updatedAt
     );
   }
 }
--- lib/screens/nutrition_screen.dart
+++ lib/screens/nutrition_screen.dart
@@ -1,7 +1,10 @@
+import 'package:fitmate/screens/logFoodManually.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../widgets/bottom_nav_bar.dart';
-import 'camera_page.dart';  // Add this import
+import 'camera_page.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class NutritionPage extends StatefulWidget {
   const NutritionPage({Key? key}) : super(key: key);
@@ -12,11 +15,153 @@ class NutritionPage extends StatefulWidget {
 
 class _NutritionPageState extends State<NutritionPage> {
   int _selectedIndex = 2;
+  double _totalCalories = 0;
+  double _totalCarbs = 0;
+  double _totalProtein = 0;
+  double _totalFat = 0;
+  Map<String, double> _dailyMacros = {};
+  String _gender = '';
+  double _weight = 0;
+  double _height = 0;
+  int _age = 0;
+  String _goal = '';
+  int _workoutDays = 0;
+  bool _isLoading = true;
 
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
     });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+  }
+
+  Future<void> _loadData() async {
+    setState(() {
+      _isLoading = true;
+    });
+    await _loadUserData();
+    await _loadFoodLogs();
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  Future<void> _loadUserData() async {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      DocumentSnapshot userData = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(user.uid)
+          .get();
+
+      if (userData.exists) {
+        setState(() {
+          _gender = userData['gender'];
+          _weight = userData['weight'];
+          _height = userData['height'];
+          _age = userData['age'];
+          _goal = userData['goal'];
+          _workoutDays = userData['workoutDays'];
+          _dailyMacros = _calculateMacronutrients(_goal, _calculateBMR(), _workoutDays);
+        });
+      }
+    }
+  }
+
+  double _calculateBMR() {
+    if (_gender.toLowerCase() == 'male') {
+      return (10 * _weight) + (6.25 * _height) - (5 * _age) + 5;
+    } else {
+      return (10 * _weight) + (6.25 * _height) - (5 * _age) - 161;
+    }
+  }
+
+  double _calculateTDEE(double bmr, int workoutDays, String goal) {
+    double multiplier;
+    double cal = 0;
+    if (workoutDays == 1) {
+      multiplier = 1.2;
+    } else if (workoutDays >= 2 && workoutDays <= 3) {
+      multiplier = 1.3;
+    } else if (workoutDays >= 4 && workoutDays <= 5) {
+      multiplier = 1.5;
+    } else {
+      multiplier = 1.9;
+    }
+    if (goal == 'Weight Loss') {
+      cal = (bmr * multiplier) - 300;
+    } else {
+      cal = bmr * multiplier;
+    }
+    return cal;
+  }
+
+  Map<String, double> _calculateMacronutrients(String goal, double bmr, int workoutDays) {
+    double tdee = _calculateTDEE(bmr, workoutDays, goal);
+    Map<String, double> macros = {};
+
+    switch (goal) {
+      case 'Weight Loss':
+        macros = {
+          'calories': tdee,
+          'carbs': (tdee * 0.45) / 4,
+          'protein': (tdee * 0.30) / 4,
+          'fat': (tdee * 0.25) / 9,
+        };
+        break;
+      case 'Gain Muscle':
+        macros = {
+          'calories': tdee,
+          'carbs': (tdee * 0.45) / 4,
+          'protein': (tdee * 0.35) / 4,
+          'fat': (tdee * 0.20) / 9,
+        };
+        break;
+      case 'Improve Fitness':
+        macros = {
+          'calories': tdee,
+          'carbs': (tdee * 0.60) / 4,
+          'protein': (tdee * 0.15) / 4,
+          'fat': (tdee * 0.25) / 9,
+        };
+        break;
+    }
+    return macros;
+  }
+
+  Future<void> _loadFoodLogs() async {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      DateTime now = DateTime.now();
+      DateTime today = DateTime(now.year, now.month, now.day);
+      DateTime tomorrow = today.add(const Duration(days: 1));
+
+      QuerySnapshot foodLogs = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(user.uid)
+          .collection('foodLogs')
+          .where('date', isGreaterThanOrEqualTo: today)
+          .where('date', isLessThan: tomorrow)
+          .get();
+
+      setState(() {
+        _totalCalories = 0;
+        _totalCarbs = 0;
+        _totalProtein = 0;
+        _totalFat = 0;
+        for (var doc in foodLogs.docs) {
+          _totalCalories += doc['calories'] ?? 0;
+          _totalCarbs += doc['carbs'] ?? 0;
+          _totalProtein += doc['protein'] ?? 0;
+          _totalFat += doc['fat'] ?? 0;
+        }
+      });
+    }
   }
 
   @override
@@ -33,41 +178,82 @@ class _NutritionPageState extends State<NutritionPage> {
         iconTheme: const IconThemeData(color: Colors.black),
         automaticallyImplyLeading: false,
       ),
-      body: SingleChildScrollView(
-        child: SafeArea(
+      body: _isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : SingleChildScrollView(
+          child: SafeArea(
           child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                // Macro circles grid
-                GridView.count(
-                  physics: NeverScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 16,
-                  mainAxisSpacing: 16,
-                  children: [
-                    _buildMacroCircle('Carbs', '225', 'g', Colors.orange, 0.75),
-                    _buildMacroCircle('Protein', '34', 'g', Colors.teal, 0.3),
-                    _buildMacroCircle('Fat', '27', 'g', Colors.pink, 0.4),
-                    _buildMacroCircle('Calories', '1399', 'Kcal', Colors.lime, 0.6),
-                  ],
-                ),
-                // Log food button
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 24.0),
-                  child: ElevatedButton(  // Changed from TextButton to ElevatedButton
+          padding: const EdgeInsets.all(16.0),
+      child: Column(
+          children: [
+          GridView.count(
+          physics: NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      crossAxisCount: 2,
+      crossAxisSpacing: 16,
+      mainAxisSpacing: 16,
+      children: [
+      _buildMacroCircle(
+      'Carbs',
+      _totalCarbs.toStringAsFixed(0),
+      'g',
+      Colors.orange,
+      _dailyMacros.containsKey('carbs')
+          ? (_totalCarbs / _dailyMacros['carbs']!).clamp(0, 1)
+          : 0,
+      _dailyMacros['carbs']?.toStringAsFixed(0) ?? '0',
+    ),
+    _buildMacroCircle(
+    'Protein',
+    _totalProtein.toStringAsFixed(0),
+    'g',
+    Colors.teal,
+    _dailyMacros.containsKey('protein')
+    ? (_totalProtein / _dailyMacros['protein']!).clamp(0, 1)
+        : 0,
+    _dailyMacros['protein']?.toStringAsFixed(0) ?? '0',
+    ),
+    _buildMacroCircle(
+    'Fat',
+    _totalFat.toStringAsFixed(0),
+    'g',
+    Colors.pink,
+    _dailyMacros.containsKey('fat')
+    ? (_totalFat / _dailyMacros['fat']!).clamp(0, 1)
+        : 0,
+    _dailyMacros['fat']?.toStringAsFixed(0) ?? '0',
+    ),
+    _buildMacroCircle(
+    'Calories',
+    _totalCalories.toStringAsFixed(0),
+    'Kcal',
+    Colors.lime,
+    _dailyMacros.containsKey('calories')
+    ? (_totalCalories / _dailyMacros['calories']!).clamp(0, 1)
+        : 0,
+    _dailyMacros['calories']?.toStringAsFixed(0) ?? '0',
+    ),
+      ],
+          ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 24.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
                     onPressed: () {
                       Navigator.push(
                         context,
                         MaterialPageRoute(
                           builder: (context) => CameraPage(),
                         ),
-                      );
+                      ).then((_) {
+                        _loadData();
+                      });
                     },
                     style: ElevatedButton.styleFrom(
                       backgroundColor: const Color(0xFFD2EB50),
-                      minimumSize: const Size(double.infinity, 50),
+                      minimumSize: const Size(150, 50),
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(5.0),
                       ),
@@ -77,74 +263,99 @@ class _NutritionPageState extends State<NutritionPage> {
                       style: GoogleFonts.bebasNeue(fontSize: 20, color: Colors.white),
                     ),
                   ),
-                ),
-                // Food suggestion card
-                Card(
-                  elevation: 4,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'Food Suggestion',
-                                style: GoogleFonts.montserrat(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              SizedBox(height: 8),
-                              Text(
-                                'Strawberries are low in calories and will fit nicely in your current plan!',
-                                style: GoogleFonts.dmSans(
-                                  color: Colors.black54,
-                                  fontSize: 14,
-                                ),
-                              ),
-                            ],
-                          ),
+                  const SizedBox(width: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => LogFoodManuallyScreen(),
                         ),
-                        SizedBox(width: 16),
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(8),
-                          child: Image.asset(
-                            'assets/data/images/strawberry.png',
-                            width: 60,
-                            height: 60,
-                            fit: BoxFit.cover,
-                            errorBuilder: (context, error, stackTrace) {
-                              return Container(
-                                width: 60,
-                                height: 60,
-                                color: Colors.grey[300],
-                                child: Icon(Icons.restaurant, color: Colors.grey),
-                              );
-                            },
-                          ),
-                        ),
-                      ],
+                      ).then((_) {
+                        _loadData();
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFFD2EB50),
+                      minimumSize: const Size(150, 50),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                    ),
+                    child: Text(
+                      'ADD FOOD',
+                      style: GoogleFonts.bebasNeue(fontSize: 20, color: Colors.white),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
+            Card(
+              elevation: 4,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Food Suggestion',
+                            style: GoogleFonts.montserrat(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            'Strawberries are low in calories and will fit nicely in your current plan!',
+                            style: GoogleFonts.dmSans(
+                              color: Colors.black54,
+                              fontSize: 14,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 16),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(8),
+                      child: Image.asset(
+                        'assets/data/images/strawberry.png',
+                        width: 60,
+                        height: 60,
+                        fit: BoxFit.cover,
+                        errorBuilder: (context, error, stackTrace) {
+                          return Container(
+                            width: 60,
+                            height: 60,
+                            color: Colors.grey[300],
+                            child: Icon(Icons.restaurant, color: Colors.grey),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+      ),
           ),
-        ),
+          ),
       ),
-      bottomNavigationBar: BottomNavBar(
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-      ),
+    bottomNavigationBar: BottomNavBar(
+    currentIndex: _selectedIndex,
+    onTap: _onItemTapped,
+    ),
     );
   }
 
-  Widget _buildMacroCircle(String label, String value, String unit, Color color, double progress) {
+  Widget _buildMacroCircle(String label, String value, String unit, Color color, double progress, String max) {
     return Container(
       padding: EdgeInsets.all(16),
       decoration: BoxDecoration(
@@ -194,6 +405,13 @@ class _NutritionPageState extends State<NutritionPage> {
             style: GoogleFonts.montserrat(
               fontSize: 14,
               fontWeight: FontWeight.w500,
+            ),
+          ),
+          Text(
+            '(max: $max)',
+            style: GoogleFonts.dmSans(
+              fontSize: 10,
+              color: Colors.grey,
             ),
           ),
         ],
